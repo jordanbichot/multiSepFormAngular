a5b25e6f588e953eacc13729f85974450aca34b3
Jordan Bichot <jordan.bichot97@gmail.com>
2023-03-31 13:12:29 -0400

    Image Working Use of Observables in template

--- src/app/steps-sidebar/steps.module.ts
+++ src/app/steps-sidebar/steps.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StepsSidebarComponent } from './steps-sidebar.component';
 import { StepControlService } from './data-access/step-control.service';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [StepsSidebarComponent],
-  imports: [AppRoutingModule, CommonModule, BrowserModule],
+  imports: [AppRoutingModule, CommonModule],
   exports: [StepsSidebarComponent],
   providers: [StepControlService],
 })
